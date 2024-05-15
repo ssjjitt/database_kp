26fcfa1a0caa98340f5b098d6966c0743e32cf30
ssjjitt <120317828+ssjjitt@users.noreply.github.com>
2024-05-16 00:33:51 +0300

    Add files via upload

---
+++ MobileBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using MobileBank.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MobileBank
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}
